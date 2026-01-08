86e99ed9702217822a8e2618f0fde106c809977c
ahmu83 <ahmu83@gmail.com>
2026-01-08 18:38:57 -0500

    udpates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    // prefix: 'tw-',
-    content: [
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    ],
+  content: [
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
     },
+  },
 
-    plugins: [forms],
+  plugins: [
+    forms,
+    daisyui,
+  ],
 };
-
